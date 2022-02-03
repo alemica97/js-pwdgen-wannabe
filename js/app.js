2538724101dccdb8cbec4ab0486716b65549219e
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-03 16:51:40 +0100

    creazione della variabile userName e unione stringhe per la generazione della password

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//Salvo il nome dell'utente in una variabile
+const userName = prompt('Come ti chiami?')
+console.log(userName)
+
+//collego le stringhe in una unica
+document.getElementById('psw').innerHTML += ' ' + userName
