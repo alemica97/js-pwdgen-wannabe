caf2fb8062efa1d61bf62413fe0fae6b4d627bca
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-03 17:24:50 +0100

    aggiunta dei punto e virgola

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 //creo una variabile costante alla quale do io il valore
-const pswNumber = 21
-console.log(pswNumber)
+const pswNumber = 21;
+console.log(pswNumber);;
 
 //Salvo il nome dell'utente in una variabile
-const userName = prompt('Come ti chiami?')
-console.log(userName)
+const userName = prompt('Come ti chiami?');
+console.log(userName);
 
 //Salvo il cognome dell'utente in una variabile
-const userSurname = prompt('Qual è il tuo cognome?')
-console.log(userSurname)
+const userSurname = prompt('Qual è il tuo cognome?');
+console.log(userSurname);
 
 //Salvo il colore preferito dell'utente in una variabile
-const favoriteColor = prompt('Qual è il tuo colore preferito?')
-console.log(favoriteColor)
+const favoriteColor = prompt('Qual è il tuo colore preferito?');
+console.log(favoriteColor);
+
 
 //collego le stringhe in una unica
 document.getElementById('psw').innerHTML += `
