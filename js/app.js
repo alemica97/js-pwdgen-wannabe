08a4fe7e334de19c7ea911b2be3e1c8b24e5a3d9
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-03 17:03:19 +0100

    Ho aggiunto tutte le variabili, queste le ho poi unite allo span nell'HTML sommandro tra loro le stringhe utilizzando il Template Literal

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
+//creo una variabile costante
+const pswNumber = 21
+console.log(pswNumber)
+
 //Salvo il nome dell'utente in una variabile
 const userName = prompt('Come ti chiami?')
 console.log(userName)
 
+//Salvo il cognome dell'utente in una variabile
+const userSurname = prompt('Qual è il tuo cognome?')
+console.log(userSurname)
+
+//Salvo il colore preferito dell'utente in una variabile
+const favoriteColor = prompt('Qual è il tuo colore preferito?')
+console.log(favoriteColor)
+
 //collego le stringhe in una unica
-document.getElementById('psw').innerHTML += ' ' + userName
+document.getElementById('psw').innerHTML += `
+                                            ${userName}${userSurname}${favoriteColor}${pswNumber}
+                                            `
